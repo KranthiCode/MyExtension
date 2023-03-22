d705bd1a54a7febb3e78971cfb3591fbf67bf5f2
kranthi <kranthi@kranthicode>
2023-03-22 20:48:25 +0530

    updated

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ openInScreener = function(word){
 };
 
 chrome.contextMenus.create({
+  id: "openInScreener",
   title: "Open in Screener",
   contexts:["selection"],
-  onclick: openInScreener
 });
 
 openInTradingView = function(word){
@@ -15,9 +15,9 @@ openInTradingView = function(word){
 };
 
 chrome.contextMenus.create({
+  id: "openInTradingView",
   title: "Open in TradingView",
   contexts:["selection"],
-  onclick: openInTradingView
 });
 
 openInEpoch = function(word){
@@ -25,35 +25,32 @@ openInEpoch = function(word){
 };
 
 chrome.contextMenus.create({
+  id: "openInEpoch",
   title: "Check in Epoch",
   contexts:["selection"],
-  onclick: openInEpoch
+});
+
+openYoutubeResults = function(word){
+  chrome.tabs.create({url: "https://www.youtube.com/results?search_query="+word.selectionText});
+};
+
+chrome.contextMenus.create({
+  id: "openYoutubeResults",
+  title: "Search Youtube",
+  contexts:["selection"],
 });
 
 openProtoViewFromLink = function(word){
   var query = word.linkUrl.replace(/view/g,"proto");
+  query = query.replace(/offer-porsche/g, "offer");
+  query = query.replace(/voucher/g,"offer");
   chrome.tabs.create({url: query, active:false});
 };
 
 chrome.contextMenus.create({
+  id: "openProtoViewFromLink",
   title: "Open Proto View",
   contexts:["link"],
-  onclick: openProtoViewFromLink
-});
-
-openProtoViewFromPage = function(word){
-  var query = word.pageUrl.replace(/view/g,"proto");
-  query = query.replace(/voucher/g,"offer");
-  console.log(query);
-  chrome.tabs.update(undefined,{url: query});
-  // chrome.tabs.create({url: "https://www.tradingview.com/chart?symbol=NSE:" + query});
-  // //chrome.tabs.create({url: "https://en.wikipedia.org/w/index.php?search=" + query + "&title=Special%3ASearch&go=Go"});
-};
-
-chrome.contextMenus.create({
-  title: "Open Proto View",
-  contexts:["page"],
-  onclick: openProtoViewFromPage
 });
 
 openOfferViewFromPage = function(word){
@@ -63,15 +60,53 @@ openOfferViewFromPage = function(word){
 };
 
 chrome.contextMenus.create({
+  id: "openOfferViewFromPage",
   title: "Open Offer View",
   contexts:["page"],
-  onclick: openOfferViewFromPage
+});
+
+openProtoViewFromPage = function(word){
+  var query = word.pageUrl.replace(/view/g,"proto");
+  query = query.replace(/offer-porsche/g, "offer");
+  query = query.replace(/voucher/g,"offer");
+  chrome.tabs.update(undefined,{url: query});
+};
+
+chrome.contextMenus.create({
+  id: "openProtoViewFromPage",
+  title: "Open Proto View",
+  contexts:["page"],
+});
+
+chrome.contextMenus.onClicked.addListener(function(info,tab){
+  switch(info.menuItemId) {
+    case 'openInScreener':
+      openInScreener(info);
+      break;
+    case 'openInTradingView':
+      openInTradingView(info);
+      break;
+    case 'openInEpoch':
+      openInEpoch(info);
+      break;
+    case 'openYoutubeResults':
+      openYoutubeResults(info);
+      break;
+    case 'openProtoViewFromLink':
+      openProtoViewFromLink(info);
+      break;
+    case 'openOfferViewFromPage':
+      openOfferViewFromPage(info);
+      break;
+    case 'openProtoViewFromPage':
+      openProtoViewFromPage(info);
+      break;
+  }
 });
 
 chrome.commands.onCommand.addListener(function(command) {
   switch (command) {
     case 'open_proto':
-      console.log("Open Proto called");
       chrome.tabs.query({currentWindow: true, active: true}, function(results) {
         if (results.length != 1) return;
         var tab = results[0];
